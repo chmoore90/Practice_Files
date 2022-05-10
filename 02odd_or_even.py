7430948d6ff7790d30faec95ceba4659a3a2f8e8
chmoore90 <chmoore90@gmail.com>
2022-05-09 23:54:39 -0400

    Added extra 2 exercise

--- 02odd_or_even.py
+++ 02odd_or_even.py
@@ -6,3 +6,21 @@ if num%2 == 0:
     print(f"{num} is even.")
 elif num%2 != 0:
     print(f"{num} is odd.")
+
+# Extra 1:
+# If the number is a multiple of 4, print out a different message.
+
+if num%4 == 0:
+    print(f"{num} is also divisible by 4.")
+
+# Extra 2:
+# Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
+# If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+num1 = int(input("Enter a new number: "))
+check = int(input("Enter another number: "))
+
+if num1%check == 0:
+    print(f"{check} divides evenly into {num1}.")
+elif num1%check != 0:
+    print(f"{check} does not divide evenly into {num1}.")
