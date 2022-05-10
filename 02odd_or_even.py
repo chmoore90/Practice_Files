5fb8a42733db08591027185eaa7c19a321c8b0a4
chmoore90 <chmoore90@gmail.com>
2022-05-09 23:39:56 -0400

    Added code

--- 02odd_or_even.py
+++ 02odd_or_even.py
@@ -1,1 +1,8 @@
 # Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
+
+num = int(input("Enter a number: "))
+
+if num%2 == 0:
+    print(f"{num} is even.")
+elif num%2 != 0:
+    print(f"{num} is odd.")
