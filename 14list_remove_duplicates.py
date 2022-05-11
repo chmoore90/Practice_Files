f7cef3e392d888adcd5aa82e5f6a9b6b043a2f57
chmoore90 <chmoore90@gmail.com>
2022-05-11 11:27:23 -0400

    started, finished

---
+++ 14list_remove_duplicates.py
@@ -0,0 +1,24 @@
+# Write a program (function!) that takes a list and returns a new list that contains all the elements
+# of the first list minus all the duplicates.
+
+import random
+no_doubles = []
+rand_list = []
+n = 15
+def remove_doubles(*list):
+    for x in a:
+        if x not in no_doubles:
+            no_doubles.append(x)
+        elif x in no_doubles:
+            continue
+    return no_doubles
+
+def gen_rand_list(n, list):
+    for x in range(n):
+        list.append(random.randint(1,10))
+    return list
+
+a = gen_rand_list(n, rand_list)
+
+print(a)
+print(remove_doubles(sorted(a)))
