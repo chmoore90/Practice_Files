3467163d683f6b96f12e849bbdd518f3dcdf9af1
chmoore90 <chmoore90@gmail.com>
2022-05-14 15:10:52 -0400

    Saved

--- 16element_search.py
+++ 16element_search.py
@@ -18,18 +18,10 @@ def rand_num_generator():
 
 rand_list = rand_list_generator(size, rand_list)
 num = rand_num_generator()
-
 print(rand_list)
 print(num)
 
-if num in rand_list:
-    print(f"Yes, {num} is in the list.")
-else:
-    print(f"Sorry, {num} is not listed.")
-
-# Extra 1: Use binary search
-
-def binary_search(num, list):
+def search_list(num, list):
     list_length = len(list)
     for x in range(list_length):
         if list[x] != num:
@@ -38,9 +30,58 @@ def binary_search(num, list):
             return True
     return False
 
-result = binary_search(num, rand_list)
+result = search_list(num, rand_list)
 
 if result is True:
-    print(f"Yes, {num} is in the list.")
+    print(f"Search result: Yes, {num} is in the list.")
 else:
-    print(f"Sorry, {num} is not listed.")
+    print(f"Search result: Sorry, {num} is not listed.")
+
+
+# Extra 1: Use binary search (both iterative and recursive)
+
+def binary_iterative(num, list):
+    low = 0
+    mid = 0
+    high = len(list) - 1
+
+    if num < list[low]:
+        return False
+    if num > list[high]:
+        return False
+    else:
+        while high >= low:
+            mid = (high + low) // 2
+            if num > list[mid]:
+                low = mid + 1
+            elif num < list[mid]:
+                high = mid -1
+            else:
+                return True
+        return False
+
+result_it = binary_iterative(num, rand_list)
+
+if result_it is True:
+    print(f"Iterative result: Yes, {num} is in the list.")
+else:
+    print(f"Iterative result: Sorry, {num} is not listed.")
+
+
+def binary_recursive(num, list, low, high):
+    if high >= low:
+        mid = (high + low) // 2
+        if num == list[mid]:
+            return True
+        elif num > list[mid]:
+            return binary_recursive(num, list, mid + 1, high)
+        else:
+            return binary_recursive(num, list, low, mid - 1)
+    return False
+
+result_rec = binary_recursive(num, rand_list, 0, len(rand_list) - 1)
+
+if result_rec is True:
+    print(f"Recursive result: Yes, {num} is in the list.")
+else:
+    print(f"Recursive result: Sorry, {num} is not listed.")
