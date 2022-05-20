ceaa5777ac9ce914bbae7259f9e973a29c4e8ab3
chmoore90 <chmoore90@gmail.com>
2022-05-20 14:14:54 -0400

    Made welcome function in tic tac toe game (27)

--- 27tic_tac_tie_draw.py
+++ 27tic_tac_tie_draw.py
@@ -1,42 +1,76 @@
 # Write code so players can play tic tac toe. When a player (say player 1, who is X) wants to place an X on the screen,
 # ask the user for a coordinate of where they want to place their piece.
 
-# From exercise 24:
 
 
-def top():
-    return ''.join([" --- --- ---", "\n", "| game[0][0]"
+reset = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+game = reset
 
 
-def one_mid():
-    return "|"
+def top(game):
+    return f" --- --- ---\n| {game[0][0]} | {game[0][1]} | {game[0][2]} |"
 
 
-def vert():
-    return f" {game[x]} |"
+def top_start():
+    return f"      ----- ----- -----\n   3 |(1,3)|(2,3)|(3,3)|"
 
 
-def create_top(x):
-    return "".join([hor() * x, "\n", one_mid(), vert() * x, "\n", hor() * x])
+def mid(game):
+    return f"\n --- --- ---\n| () | {game[1][1]} | {game[1][2]} |"
 
 
-def create_bot(x):
-    return "".join(["\n", one_mid(), vert() * x, "\n", hor() * x])
+def mid_start():
+    return f"\n      ----- ----- -----\nY: 2 |(2,1)|(2,2)|(3,2)|"
 
 
-def create_board(x, y):
-    board = "".join([create_top(x), (create_bot(x)) * (y - 1)])
-    return board
+def bot(game):
+    return f"\n --- --- ---\n| {game[2][0]} | {game[2][1]} | {game[2][2]} |"
 
 
-columns = 3
-rows = 3
-x = " "
-b = create_board(columns, rows)
+def bot_start():
+    return f"\n      ----- ----- -----\n   1 |(1,1)|(2,1)|(3,1)|"
 
 
-print(b)
+def draw(game):
+    return "".join(list((top(game), mid(game), bot(game))))
 
-game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+game_board = {
+    ""
+}
 
-print(game)
+
+# def welcome():
+#     print(
+#         "Welcome to Chloe's Tic Tac Toe!\nThe game board uses (x,y) coordinates as input to place your markers:"
+#     )
+#     print(
+#         "".join(
+#             list(
+#                 [
+#                     top_start(),
+#                     mid_start(),
+#                     bot_start(),
+#                     "\n      ----- ----- -----",
+#                     "\nX:      1     2     3",
+#                 ]
+#             )
+#         )
+#     )
+#     print("Enter your coordinates as 'x,y'. Don't include any spaces or parentheses.")
+
+
+# welcome()
+
+# ready = input("Are you ready to play? (Type XXX to exit) ")
+# while ready != "XXX":
+#     pass
+
+
+def play():
+    coors = input("Enter your coordinates: ")
+    coorl = coors.split(',')
+    x = coorl[0]
+    y = coorl[1]
+    print(x,y)
+
+play()
