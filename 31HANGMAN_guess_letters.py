020c1a71ef6bfaa91a467f577ea813f4f380a676
chmoore90 <chmoore90@gmail.com>
2022-05-29 12:25:28 -0400

    Added logic to identify guess letters in word (31HANG)

---
+++ 31HANGMAN_guess_letters.py
@@ -0,0 +1,42 @@
+# For this exercise, write the logic that asks a player to guess a letter and displays letters in the clue word that were
+# guessed correctly. For now, let the player guess an infinite number of times until they get the entire word. As a bonus,
+# keep track of the letters the player guessed and display a different message if the player tries to guess that letter again.
+# Remember to stop the game when all the letters have been guessed correctly! Don’t worry about choosing a word randomly or
+# keeping track of the number of guesses the player has remaining - we will deal with those in a future exercise.
+
+import random
+
+
+def get_word():
+    with open("sowpods.txt", "r") as sowpods_dict:
+        sowpods = sowpods_dict.read()
+    word = "".join(random.sample(sowpods.split(), 1))
+    return word
+
+
+word = get_word()
+print(word)
+
+game_letters = "_" * len(word)
+print(game_letters)
+
+while True:
+    guess = input("Guess a letter: ").upper()
+
+    if guess not in word:
+        print(f"no {guess}")
+        continue
+
+    index = []
+
+    for x in range(len(word)):
+        try:
+            index.append(word.index(guess, x, x+1))
+        except:
+            pass
+    print(index)
+
+    for i in index:
+        game_letters = game_letters[i].replace("_", guess)
+
+    print(game_letters)
