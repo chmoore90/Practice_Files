b4419012f7c232dbc2d88c4d2562a07fed537496
chmoore90 <chmoore90@gmail.com>
2022-05-31 14:51:13 -0400

    Added final hangman file (draft). Renamed TTT final version

---
+++ 32HANGMAN.py
@@ -0,0 +1,87 @@
+# In this exercise, we will finish building Hangman. In the game of Hangman, the player only has 6 incorrect guesses
+# (head, body, 2 legs, and 2 arms) before they lose the game. In Part 1, we loaded a random word list and picked a word
+# from it. In Part 2, we wrote the logic for guessing the letter and displaying that information to the user.
+# In this exercise, we have to put it all together and add logic for handling guesses.
+# Copy your code from Parts 1 and 2 into a new file as a starting point. Now add the following features:
+# Only let the user guess 6 times, and tell the user how many guesses they have left.
+# Keep track of the letters the user guessed. If the user guesses a letter they already guessed, don’t penalize them - let them guess again.
+# Optional additions:
+# When the player wins or loses, let them start a new game.
+# Rather than telling the user "You have 4 incorrect guesses left", display some picture art for the Hangman.
+# This is challenging - do the other parts of the exercise first!
+# Your solution will be a lot cleaner if you make use of functions to help you!
+
+
+import random
+
+
+def get_word():
+    with open("sowpods.txt", "r") as sowpods_dict:
+        sowpods = sowpods_dict.read()
+    word = "".join(random.sample(sowpods.split(), 1))
+    return word
+
+
+def check_guess(guess, word, game_letters):
+    game_letters = list(game_letters)
+    index = []
+
+    for x in range(len(word)):
+        try:
+            index.append(word.index(guess, x, x + 1))
+        except:
+            pass
+
+    for i in index:
+        game_letters.pop(i)
+        game_letters.insert(i, guess)
+
+    game_letters = "".join(game_letters)
+
+    return game_letters
+
+
+while True:
+    word = get_word()
+    game_letters = "_" * len(word)
+    guess_list = []
+
+    while True:
+
+        guess = input(
+            f"Previous guesses: {', '.join(guess_list)}\nGuess a letter: "
+        ).upper()
+
+        if guess in guess_list:
+            print(f"You've already guessed '{guess}'.")
+            print(game_letters)
+            continue
+
+        if len(guess) != 1:
+            print("Invalid guess. Please guess one letter.")
+            continue
+
+        if guess.isalpha() is not True:
+            print("Invalid guess. Please enter a letter.")
+            continue
+
+        if guess not in word:
+            print(f"no {guess}")
+            print(game_letters)
+            guess_list.append(guess)
+            continue
+
+        guess_list.append(guess)
+
+        new_letters = check_guess(guess, word, game_letters)
+
+        game_letters = new_letters
+        if game_letters == word:
+            print(f"Congrats you got it! The word was {word}.")
+            replay = input("Do you want to play again? ")
+            if replay.strip().lower() not in ["y", "yes", "ready", "ok", "1", "true"]:
+                print("Thanks for playing!")
+                exit()
+            break
+
+        print(game_letters)
